fbaeb5f1784b2f7f69f15283a75b64b793425cbf
yeonnex <yeonnex@gmail.com>
2023-08-25 23:43:21 +0900

    create front-end scaffold

---
+++ front-end/tests/e2e/specs/test-with-pageobjects.js
@@ -0,0 +1,11 @@
+/// /////////////////////////////////////////////////////////////
+// For authoring Nightwatch tests, see
+// https://nightwatchjs.org/guide
+//
+// For more information on working with page objects see:
+//   https://nightwatchjs.org/guide/working-with-page-objects/
+/// /////////////////////////////////////////////////////////////
+
+module.exports = {
+
+}
