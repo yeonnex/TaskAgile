3178f1601b793dc16adbee28fcd01794f0dd1f01
yeonnex <yeonnex@gmail.com>
2023-08-26 08:22:02 +0900

    bridge communication - 백엔드와 프론트엔드 통신 - '/api/*' 요청 프록시로 연결 - 크로스 오리진 요청 제한 해결

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -2,6 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    port: 3000
+    port: 3000,
+    proxy: {
+      '/api/*': {
+        target: 'http://localhost:8080'
+      }
+    }
   }
 })
