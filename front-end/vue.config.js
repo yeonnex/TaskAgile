476fbbfcaa81151f07dc7ccd4e387b7473eb6332
yeonnex <yeonnex@gmail.com>
2023-08-27 22:07:35 +0900

    feat: bootstrap 추가 및 적용

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -8,5 +8,13 @@ module.exports = defineConfig({
         target: 'http://localhost:8080'
       }
     }
+  },
+  configureWebpack: {
+    entry: {
+      app: './src/main.js',
+      style: [
+        'bootstrap/dist/css/bootstrap.min.css'
+      ]
+    }
   }
 })
