9fd50cc7bf932177c931a099a641300659b42c4d
yeonnex <yeonnex@gmail.com>
2023-08-27 23:14:05 +0900

    feat: implement UI of register page

---
+++ front-end/src/views/RegisterPage.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="register-form">
+        <div class="logo-wrapper">...</div>
+        <form>
+          <div class="form-group"></div>
+          <div class="form-group"></div>
+          <div class="form-group"></div>
+          <button type="submit" class="btn btn-primary"></button>
+        </form>
+      </div>
+    </div>
+
+    <footer class="footer">
+      <span>...</span>
+      <ul class="list-inline justify-content-end"></ul>
+    </footer>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  max-width: 900px;
+}
+.register-form {
+  margin-top: 50px;
+  max-width: 320px;
+}
+.logo-wrapper {
+  margin-bottom: 40px;
+}
+.footer {
+  width: 100%; line-height: 40px; margin-top: 50px;
+}
+</style>
