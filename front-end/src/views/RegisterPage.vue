73cfb71f430fbb006fdf0e3497737ade0a350531
yeonnex <yeonnex@gmail.com>
2023-08-29 00:56:52 +0900

    feat: 폼 바인딩 구현, 테스트 코드 작성

--- front-end/src/views/RegisterPage.vue
+++ front-end/src/views/RegisterPage.vue
@@ -1,5 +1,22 @@
-<script setup>
+<script>
 
+export default {
+  name: 'RegisterPage',
+  data: function () {
+    return {
+      form: {
+        username: '',
+        emailAddress: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    submitForm() {
+
+    }
+  }
+}
 </script>
 
 <template>
@@ -7,11 +24,17 @@
     <div class="row justify-content-center">
       <div class="register-form">
         <div class="logo-wrapper">...</div>
-        <form>
-          <div class="form-group"></div>
-          <div class="form-group"></div>
-          <div class="form-group"></div>
-          <button type="submit" class="btn btn-primary"></button>
+        <form @submit.prevent="submitForm">
+          <div class="form-group">
+            <input id="username" v-model="form.username" class="form-control" type="text">
+          </div>
+          <div class="form-group">
+            <input id="emailAddress" v-model="form.emailAddress" class="form-control" type="email">
+          </div>
+          <div class="form-group">
+            <input id="password" v-model="form.password" class="form-control" type="password">
+          </div>
+          <button class="btn btn-primary" type="submit"></button>
         </form>
       </div>
     </div>
@@ -23,18 +46,23 @@
   </div>
 </template>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
 .container {
   max-width: 900px;
 }
+
 .register-form {
   margin-top: 50px;
   max-width: 320px;
 }
+
 .logo-wrapper {
   margin-bottom: 40px;
 }
+
 .footer {
-  width: 100%; line-height: 40px; margin-top: 50px;
+  width: 100%;
+  line-height: 40px;
+  margin-top: 50px;
 }
 </style>
--- front-end/tests/unit/RegisterPage.spec.js
+++ front-end/tests/unit/RegisterPage.spec.js
@@ -1,28 +1,30 @@
-import Vue from 'vue'
 import RegisterPage from '@/views/RegisterPage.vue';
+import {mount} from '@vue/test-utils';
 
 describe('RegisterPage.vue', () => {
-  it('should render correct contents', () => {
-    const Constructor =
-      Vue.extend(RegisterPage);
-    const vm = new Constructor().$mount();
+  let wrapper;
+  let fieldUsername;
+  let fieldEmailAddress;
+  let fieldPassword;
+  let buttonSubmit;
 
-    expect(vm.$el.querySelector('.logo').getAttribute('src'))
-      .toEqual('/static/images/logo.png');
+  beforeEach(() => {
+    wrapper = mount(RegisterPage);
 
-    expect(vm.$el.querySelector('.tagline').textContent)
-      .toEqual('Open source task management tool');
-
-    expect(vm.$el.querySelector('#username').value)
-      .toEqual('');
-
-    expect(vm.$el.querySelector('#emailAddress').value)
-      .toEqual('');
-
-    expect(vm.$el.querySelector('#password').value)
-      .toEqual('');
-
-    expect(vm.$el.querySelector('form button[type="submit"]').textContent)
-      .toEqual('Create account');
+    fieldUsername = wrapper.find('#username');
+    fieldEmailAddress = wrapper.find('#emailAddress');
+    fieldPassword = wrapper.find('#password');
+    buttonSubmit = wrapper.find('form button[type="submit"]');
   })
+
+  it('should render registration form', () => {
+
+    expect(fieldUsername.element.value).toEqual('');
+
+    expect(fieldEmailAddress.element.value).toEqual('');
+
+    expect(fieldPassword.element.value).toEqual('');
+
+  })
+
 })
