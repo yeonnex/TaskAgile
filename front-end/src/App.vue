2174e2e167474d0ff68422beaa5cb0fa03220651
yeonnex <yeonnex@gmail.com>
2023-09-02 02:18:34 +0900

    feat: App.vue 에서 프로젝트 공용 스타일 선언, RegisterPage.vue 개발

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -4,4 +4,66 @@
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+html,
+body {
+  height: 100%;
+  font-size: 14px;
+  font-family: "Helvetica Neue", Arial, Helvetica, sans-serif !important;
+}
+
+#app,
+.page {
+  height: 100%;
+  position: relative;
+}
+
+.page {
+  display: flex;
+  flex-direction: column;
+}
+
+.public.container {
+  max-width: 900px;
+}
+
+.public {
+  .form {
+    margin-top: 50px;
+    width: 320px;
+
+    .form-group {
+      margin-top: 10px;
+      margin-bottom: 10px;
+
+      label {
+        margin-bottom: 5px;
+        font-weight: bold;
+        color: #555;
+      }
+
+      .error {
+        line-height: 1;
+        display: none;
+        margin-top: 5px;
+      }
+    }
+  }
+}
+
+.field-error {
+  .error {
+    display: block;
+    color: #ff0000;
+  }
+}
+
+input.form-control:focus,
+textarea.form-control:focus {
+  border: 1px solid #f4c2c2 !important;
+}
+
+.modal-open .modal-backdrop.show {
+  opacity: 0.7;
+}
+</style>
--- front-end/src/views/RegisterPage.vue
+++ front-end/src/views/RegisterPage.vue
@@ -39,13 +39,18 @@ export default {
 
 <template>
   <div>
-    <div class="container">
+    <div class="container public">
       <div class="row justify-content-center">
-        <div class="register-form">
+        <div class="form">
           <LogoComponent />
           <form @submit.prevent="submitForm">
-            <div v-show="errorMessage" class="alert alert-danger failed"></div>
+            <div v-show="errorMessage" class="alert alert-danger failed">
+              {{ errorMessage }}
+            </div>
             <div class="form-group">
+              <label for="username">{{
+                $t("registerPage.form.username.label")
+              }}</label>
               <input
                 id="username"
                 v-model="form.username"
@@ -54,6 +59,9 @@ export default {
               />
             </div>
             <div class="form-group">
+              <label for="email">{{
+                $t("registerPage.form.emailAddress.label")
+              }}</label>
               <input
                 id="emailAddress"
                 v-model="form.emailAddress"
@@ -62,6 +70,9 @@ export default {
               />
             </div>
             <div class="form-group">
+              <label for="password">{{
+                $t("registerPage.form.password.label")
+              }}</label>
               <input
                 id="password"
                 v-model="form.password"
@@ -69,7 +80,9 @@ export default {
                 type="password"
               />
             </div>
-            <button class="btn btn-primary" type="submit"></button>
+            <button class="btn btn-primary w-100" type="submit">
+              {{ $t("registerPage.form.submit") }}
+            </button>
           </form>
         </div>
       </div>
@@ -78,13 +91,4 @@ export default {
   </div>
 </template>
 
-<style lang="scss" scoped>
-.container {
-  max-width: 900px;
-}
-
-.register-form {
-  margin-top: 50px;
-  max-width: 320px;
-}
-</style>
+<style lang="scss" scoped></style>
